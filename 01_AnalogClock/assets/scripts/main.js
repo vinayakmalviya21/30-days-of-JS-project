cfdeddb112058208d8c908470b0fa8c7ab1aa3af
hp <vinayakmalviya21@gmail.com>
2023-09-21 12:08:14 +0530

    fifth day commit

--- 01_AnalogClock/assets/scripts/main.js
+++ 01_AnalogClock/assets/scripts/main.js
@@ -1,21 +1,21 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
-function setDate(){
-    const now = new Date();
-    
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds/60)*360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+function setDate() {
+  const now = new Date();
 
-    const mins = now.getMinutes();
-    const minsDegrees = ((mins/60)*360) + 90;
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-    const hours = now.getHours();
-    const hourDegrees = ((mins/12)*360) + 90;
-    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hourDegrees = (mins / 12) * 360 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-setInterval(setDate,1000);
+setInterval(setDate, 1000);
--- 03_DrumKit/assets/scripts/main.js
+++ 03_DrumKit/assets/scripts/main.js
@@ -3,62 +3,62 @@ import keys from "../data/keys.json" assert { type: "json" };
 
 //handle preloader
 window.addEventListener("load", () => {
-    document.querySelector(".preloader").style.display = "none";
+  document.querySelector(".preloader").style.display = "none";
 });
 
 // populating keys
 const keysWrapper = document.getElementsByClassName("keys")[0];
 
 keys.forEach((element) => {
-    const keyDiv = document.createElement("div");
-    keyDiv.classList.add("key");
-    keyDiv.setAttribute("data-key", element.keyCode);
-    const kbd = document.createElement("kbd");
-    kbd.innerText = element.letter;
-    const span = document.createElement("span");
-    span.className = "sound";
-    span.innerText = element.sound;
-    keyDiv.appendChild(kbd);
-    keyDiv.appendChild(span);
-    keysWrapper.appendChild(keyDiv);
+  const keyDiv = document.createElement("div");
+  keyDiv.classList.add("key");
+  keyDiv.setAttribute("data-key", element.keyCode);
+  const kbd = document.createElement("kbd");
+  kbd.innerText = element.letter;
+  const span = document.createElement("span");
+  span.className = "sound";
+  span.innerText = element.sound;
+  keyDiv.appendChild(kbd);
+  keyDiv.appendChild(span);
+  keysWrapper.appendChild(keyDiv);
 });
 
 // populating audios
 audio.forEach((e) => {
-    const audio = document.createElement("audio");
-    console.log(e.src)
-    audio.src = e.src;
-    audio.setAttribute("data-key", e.keyCode);
-    document.body.appendChild(audio);
+  const audio = document.createElement("audio");
+  console.log(e.src);
+  audio.src = e.src;
+  audio.setAttribute("data-key", e.keyCode);
+  document.body.appendChild(audio);
 });
 
 function playsound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing");
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 }
 function playsoundMobile(e) {
-    const keyCode = e.target.getAttribute("data-key");
-    const key = document.querySelector(`.key[data-key="${keyCode}"]`);
-    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
-    key.classList.add("playing");
+  const keyCode = e.target.getAttribute("data-key");
+  const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
 }
 
 function removeTransition(e) {
-    if (e.propertyName !== "transform") return;
-    this.classList.remove("playing");
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
 }
 
 const keyElements = document.querySelectorAll(".key");
 keyElements.forEach((key) => {
-    key.addEventListener("click", playsoundMobile);
-    key.addEventListener("transitionend", removeTransition);
+  key.addEventListener("click", playsoundMobile);
+  key.addEventListener("transitionend", removeTransition);
 });
 // listening events
 window.addEventListener("keydown", playsound);
